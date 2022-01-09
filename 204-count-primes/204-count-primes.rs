5ff5500b327c438b4f97d50eb8db773f92d4df54
Faisal Ahmed <42486737+felixfaisal@users.noreply.github.com>
2022-01-09 23:53:10 +0530

    Time: 223 ms (34.48%), Space: 6.8 MB (58.62%) - LeetHub

---
+++ 204-count-primes/204-count-primes.rs
@@ -0,0 +1,21 @@
+impl Solution {
+    pub fn count_primes(n: i32) -> i32 {
+        let n: usize = n as usize;
+        let mut not_prime = vec![false; n];
+        let mut cnt = 0;
+
+        for i in 2..n {
+            if !not_prime[i] {
+                cnt += 1;
+
+                let mut j = 2;
+                while i * j < n {
+                    not_prime[i * j] = true;
+                    j += 1;
+                }
+            }
+        }
+
+        return cnt;
+    }
+}
